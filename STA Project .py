a787ab25631180b407f310f5ca81ceb396b8fe8c
David-sin7 <davidl.le@mail.utoronto.ca>
2024-11-04 22:23:02 -0600

    Add files via upload

---
+++ STA Project .py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Individual Proposals (due Mon Nov04, immediately upon return from Reading Week)
+# [This is worth 2 course points, 11% of the project grade]
+# 
+# Each student will individually suggest three (3) distinct analyses that they plan to use to explore the CSCS data
+# 
+# Describe (a) the variables you plan to explore, and (b) the analysis you plan to perform
+# Describe (a) the possible results your analysis may produce, and (b) how this could be relevant for the objectives of the course project
+# Include your results if you have already completed an analysis
+# Your TUT TA will review each students individual submission and create 6 groups of 3-4 students from your TUT (not students from other TUTs) based on the interests expressed in these proposals, and/or previously observed during TUT. If students have preferences about which team members they'd like to work with, they should include these along with their submitted proposals. Ideally students who are "more actively engaged" in the courese will be grouped together, while students demonstrating "less course participation" will form their own group.
+# 
+# All project assignment requirements are independent of group size. All groups, regardless of size, will be evaluated according to the same rubric. Given the nature of the Course Project and its objectives, it would be possible for even a single student to accel with respect to the rubrics of the Course Project.
+# There is a (recommended but optional) "Current Course Project Capability Level" section which guides students in applying the course topics from each week to the course project data. By Mon Nov04 students will have had exposure to the topics of Data Summarization, Visualization, Confidence Intervals, Hypothesis Testing, and Simple Linear regression, any combination of which would be appropriate choices for the proposed analyses. Student proposals don't necessarily have to be based on three different categories but a diverse proposal considering different approaches to exploring the project dataset likely would be. By Mon Nov04 students will not yet have enountered Mulitple Linear Regression or Decision Tree Classification, but they are welcome to propose analyses based on these methodologies if they are sufficiently familiar with these methodologies to do so sensibly. Students are also encouraged to integrate the considerations of the (recommended but optional) "Ethical Profesionalism Considerations" into their proposals. Addressing "ethics" in these proposals is not a required aspect of this assignment, but demonstrating an awareness of these perspectives within the proposal will certainly be viewed favorably at this stage in the coure project.
+
+# My overall question is which group of people does prefer to spend times on them
+# I more likely to group with jiongxi.he@mail.utoronto.ca and yijing.wan@mail.utoronto.ca
+
+# #1 93
+# Variables: numbers of people, hours to spend on socializing with family members
+# 
+# Analysis: I want to use a box chart to see how many times does most people prefer to chat with family members
+# 
+# Possible Result: The box chart may reveal that most people chat with family members either daily, weekly, or less frequently, depending on lifestyle and relationship dynamics. 
+# 
+# How this could be relevant to the objectives of the course project: This analysis can help identify which is total time spending on family communication in normal. Understanding these communication preferences could be relevant to course objectives offering insights into how people prioritize family connections within their daily lives.
+
+# #2  94
+# Variables: numbers of people, hours to spend on socializing with friends
+# 
+# Analysis:I want to use a box chart to see how many times does most people prefer to chat with their friends
+# 
+# Possible result: The box plot may show that most people spend a certain range of hours on socializing with a median value representing the most common preference.
+# 
+# How this could be relevant to the objectives of the course project: This analysis can help identify which is total time spending on socializing with friends. Understanding these communication preferences could be relevant to course objectives offering insights into how people prioritize friends connections within their daily lives.
+
+# #3  95
+# Variables:numbers of people, hours to spend on socializing with classmates or coworkres
+# 
+# Analysis:I want to use a box chart to see how many times does most people prefer to chat with their classmates or coworkres
+# 
+# Possible result: I want to use a bar chart to see how many times does most people prefer to chat with their classmates. Additionally, the median of box chart may smaller than the other two. 
+# 
+# How this could be relevant to the objectives of the course project: This analysis can help identify which is total time spending on socializing with classmates or coworkers. Understanding these communication preferences could be relevant to course objectives offering insights into how people prioritize coworkeres or classmates connections within their daily lives.
+
+# 
+
+# In[ ]:
+
+
+
+
